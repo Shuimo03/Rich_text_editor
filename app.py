bf9475c9808f1d37a2fd15f13fd186b65ffc81ca
Cola <wu1998102@gmail.com>
2018-11-18 21:48:24 +0800

    更新路由，修改完成了一半

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ app = Flask(__name__)
 def text_home():
     return render_template('home.html')
 
-@app.route("/getnotices/",methods = ['GET'])
-def text_list():
-    listText = session.query(Text).all()
-    return render_template('textList.html', listText = listText)
+@app.route("/notices/<ID>",methods = ['GET'])
+def text_add(ID):
+    return render_template("addText.html",ID = Text.ID)
 
-@app.route("/postnotices/",methods = ['GET'])
-def text_add():
-    return render_template("addText.html")
-
-@app.route("/postnotices/", methods = ['POST'])
+@app.route("/notices/", methods = ['POST'])
 def text_submittal():
     add()
     listText = session.query(Text).all()
     return render_template('textList.html', listText= listText)
 
+@app.route("/notices/update/<ID>",methods = ["GET"])
+def text_update(ID):
+        test = session.query(Text).filter_by(ID =ID).first()
+        return  render_template('updateText.html',test =test)
 
+@app.route("/notices/new",methods = ['GET'])
+def text_list():
+    listText = session.query(Text).all()
+    return render_template('textList.html', listText = listText)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- route/update_text.py
+++ route/update_text.py
@@ -3,6 +3,6 @@ from DataBase.dataTable import Text,session
 
 # 获取数据
 def update():
-    updateContent = session.query(Text).filter_by(Content = Text.Content).first()
-    updateContent.Content = request.form['Content']
-
+    Text.Content = request.form['Content']
+    Text.EndShowDate = request.form['EndShowDate']
+    session.commit()
