0bbc44deada60229d6ab94bba2c0e082a8657f4c
Cola <wu1998102@gmail.com>
2018-11-20 09:25:22 +0800

    第一版已经做好，更新不会覆盖数据

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import  render_template
 from flask import Flask
 from route.add_text import add,session
-from route.update_text import update
+from route.update_text import updateText
 from DataBase.dataTable import Text
 
 app = Flask(__name__)
@@ -9,9 +9,9 @@ app = Flask(__name__)
 def text_home():
     return render_template('home.html')
 
-@app.route("/notices/<ID>",methods = ['GET'])
-def text_add(ID):
-    return render_template("addText.html",ID = Text.ID)
+@app.route("/notices/",methods = ['GET'])
+def text_add():
+    return render_template("addText.html")
 
 @app.route("/notices/", methods = ['POST'])
 def text_submittal():
@@ -19,10 +19,17 @@ def text_submittal():
     listText = session.query(Text).all()
     return render_template('textList.html', listText= listText)
 
-@app.route("/notices/update/<ID>",methods = ["GET"])
+@app.route("/notices/<ID>",methods = ["GET"])
 def text_update(ID):
-        test = session.query(Text).filter_by(ID =ID).first()
-        return  render_template('updateText.html',test =test)
+        text = session.query(Text).filter_by(ID =ID).first()
+        return  render_template('updateText.html',text =text)
+
+#在原本内容上进行修改，但是不会覆盖原来内容
+@app.route("/notices/<Content>",methods = ["POST"])
+def sumbit_update(Content):
+    updateText(Content = Content)
+    listText = session.query(Text).all()
+    return render_template('textList.html', listText=listText)
 
 @app.route("/notices/new",methods = ['GET'])
 def text_list():
--- route/update_text.py
+++ route/update_text.py
@@ -1,8 +1,17 @@
 from flask import request
 from DataBase.dataTable import Text,session
+import time
+
+
 
 # 获取数据
-def update():
-    Text.Content = request.form['Content']
-    Text.EndShowDate = request.form['EndShowDate']
+def updateText(Content):
+    nowtime = time.time()
+    nowdate = time.localtime(nowtime)
+    updateContent = request.form['updateContent']
+    updateEndShowDate = request.form['updateEndShowDate']
+    DataCreateTime = time.strftime("%Y-%m-%d-%H-%M", nowdate)
+    text = Text(Content=updateContent, DataCreateTime=DataCreateTime, EndShowDate=updateEndShowDate)
+    session.query(Text).filter_by(Content = Content).first()
+    session.add(text)
     session.commit()
